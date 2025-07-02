22f56b80bc9f7a7a8dae8a3adb5f78a7740a5b85
sandriine <neveusandrine@free.fr>
2025-07-02 21:46:45 +0200

    add id

--- src/main/java/com/sandrine/infra/controller/CoordinateResponse.java
+++ src/main/java/com/sandrine/infra/controller/CoordinateResponse.java
@@ -2,8 +2,8 @@ package com.sandrine.infra.controller;
 
 import com.sandrine.domain.model.Coordinate;
 
-public record CoordinateResponse(double latitude, double longitude) {
+public record CoordinateResponse(Long id, double latitude, double longitude) {
     public static CoordinateResponse from(Coordinate coordinate) {
-        return new CoordinateResponse(coordinate.latitude(), coordinate.longitude());
+        return new CoordinateResponse(coordinate.id(), coordinate.latitude(), coordinate.longitude());
     }
 }
